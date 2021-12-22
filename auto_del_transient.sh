325e1b4c49671a5d514ed05b2ea6e9c95c5920ff
dotrungquan <56627093+dotrungquan@users.noreply.github.com>
2021-12-22 12:05:26 +0700

    Create auto_del_transient.sh

---
+++ auto_del_transient.sh
@@ -0,0 +1,18 @@
+#script delete transient on wordpress
+#The Transients API is very similar to the Options API but with the added feature of an expiration time, 
+which simplifies the process of using the wp_options database table to temporarily store cached information.
+
+DBNAME="Nhap_vao_DB_Name"
+DBUSER="Nhap_vao_DB_User"
+DBPASSWD="Nhap_vao_mat_khau"
+MYSQLBIN=/usr/bin/mysql # OR MYSQLBIN=/usr/local/mysql/bin/mysql
+MYSQL="${MYSQLBIN} -s -D ${DBNAME} -u ${DBUSER} -p${DBPASSWD}"
+#MYSQL="mysql"
+TMP=/var/tmp/
+ENTRIES_FILE="${TMP}entries.$$"
+
+${MYSQL} <<'EOF'
+DELETE FROM `liathia_options` WHERE `option_name` LIKE ('_transient_%'); #Thay vao ten table _options
+DELETE FROM `liathia_options` WHERE `option_name` LIKE ('_site_transient_%'); #Thay vao ten table _options
+exit
+EOF
