04cfe3098ca5fec80d3ed72bcf41e4b613bb9e24
dotrungquan <56627093+dotrungquan@users.noreply.github.com>
2023-02-08 11:19:13 +0700

    Create drivedownload.sh

---
+++ drivedownload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Auth: DOTRUNGQUAN.INFO
+read -p "Nhap ID: " uid
+read -p "Nhap File Nam: " filename
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$uid'' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\n/p')&id=$uid" -O '$filename' && rm -rf /tmp/cookies.txt
