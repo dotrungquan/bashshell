0efa1895f29b9bf95126d6b6baa84b91e49ac765
dotrungquan <dotrungquan.info@gmail.com>
2023-08-11 11:32:20 +0700

    Update create-swap.sh

--- create-swap.sh
+++ create-swap.sh
@@ -1,24 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-# size of swapfile in megabytes
-swapsize=2048
+echo "Chào mừng bạn đến với DOTRUNGQUAN.INFO!"
+read -p "Nhập kích thước swap bạn muốn (theo đơn vị GB): " swap_size
 
-# does the swap file already exist?
-grep -q "swapfile" /etc/fstab
-
-# if not then create it
-if [ $? -ne 0 ]; then
-    echo 'swapfile not found. Adding swapfile.'
-    fallocate -l ${swapsize}M /swapfile
-    chmod 600 /swapfile
-    mkswap /swapfile
-    swapon /swapfile
-    echo '/swapfile none swap defaults 0 0' >> /etc/fstab
-else
-    echo 'swapfile found. No changes made.'
+# Kiểm tra xem người dùng đã nhập số hợp lệ hay chưa
+re='^[0-9]+$'
+if ! [[ $swap_size =~ $re ]]; then
+    echo "Lỗi: Bạn cần phải nhập một số nguyên dương!"
+    exit 1
 fi
 
-echo '--------------------------------------------'
-echo 'Check whether the swap space created or not?'
-echo '--------------------------------------------'
-swapon --show
+# Tạo swap với kích thước đã nhập
+echo "Đang tạo swap với kích thước $swap_size GB..."
+sudo dd if=/dev/zero of=/swapfile bs=1G count=$swap_size
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+# Thêm swap vào file /etc/fstab để tự động kích hoạt sau khi khởi động
+echo "/swapfile none swap sw 0 0" | sudo tee -a /etc/fstab
+
+echo "Swap đã được tạo thành công và đã kích hoạt!"
+echo "Thông tin swap:"
+free -h
+
+echo "Xin cảm ơn đã sử dụng script!"
